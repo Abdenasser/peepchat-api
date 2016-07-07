15f96641cfee4a547a79bc1f2580618d1808f5a7
abdenasser <nasser.elidrissi065@gmail.com>
2016-07-07 19:05:18 +0000

    My first Phoenix API

---
+++ web/router.ex
@@ -0,0 +1,13 @@
+defmodule Peepchat.Router do
+  use Peepchat.Web, :router
+
+  pipeline :api do
+    plug :accepts, ["json", "json-api"]
+  end
+
+  scope "/api", Peepchat do
+    pipe_through :api
+    
+    resources "session", SessionController, only: [:index]
+  end
+end
